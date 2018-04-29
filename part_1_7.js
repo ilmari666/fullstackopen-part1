d73ec3a4ba461cb89ca684e90d0bee0113bcffdc
Ilmari <ilmari@sokeri.com>
2018-04-29 13:09:28 +0300

    part 1.7

--- part_1_7.js
+++ part_1_7.js
@@ -15,41 +15,47 @@ const Button = (props) => {
 };
 
 const Header = ({ label }) => <h1>{label}</h1>;
+
 const Choices = ({ choices, choose }) => (<div>
   {choices.map(choice =>
     <Button type="button" onClick={() => choose(choice)} key={choice}>{OPTIONS[choice]}</Button>)}
 </div>);
 
-const getHistoryPerButton = history =>
-  Object.keys(history).map(key => <p>{`${OPTIONS[key]} ${history[key]}`}</p>);
+const getButtonClicks = history =>
+  Object.keys(history).map(key => ({
+    label: OPTIONS[key],
+    value: history[key],
+  }));
 
-const getTotalClicks = (history) => 
+const getTotalClicks = history =>
   Object.keys(history).reduce((accumulator, option) => accumulator + history[option], 0);
 
 const getPositiveClickPct = (history) => {
   const totalClicks = getTotalClicks(history);
-  if (totalClicks === 0){
-    return null;
+  if (totalClicks === 0) {
+    return 0;
   }
   const positiveClicks = history.Good;
   const avgPct = (positiveClicks / totalClicks) * 100;
-  return <p>positiivisia { avgPct.toFixed(1) } %</p>;
+  return `${avgPct.toFixed(1)}%`;
 };
 
 
 const getClickAvg = (history) => {
   const totalClicks = getTotalClicks(history);
-  if (totalClicks === 0){
-    return null;
+  if (totalClicks === 0) {
+    return 0;
   }
   const avgPoints = (history.Good - history.Bad) / totalClicks;
-  return <p>keskiarvo { avgPoints.toFixed(1) }</p>;
+  return avgPoints.toFixed(1);
 };
 
-const Stats = ({ history }) => (<div>
-  { getHistoryPerButton(history) }
-  { getClickAvg(history)}
-  { getPositiveClickPct(history) }
+const Statistic = ({ label, value }) => <p>{ label } { value }</p>;
+
+const Statistics = ({ history }) => (<div>
+  { getButtonClicks(history).map(historySet => <Statistic {...historySet} />) }
+  <Statistic label="keskiarvo" value={getClickAvg(history)} />
+  <Statistic label="positiivisia" value={getPositiveClickPct(history)} />
 </div>);
 
 class App extends Component {
@@ -75,7 +81,7 @@ class App extends Component {
         <Header label="anna palautetta" />
         <Choices choices={Object.keys(OPTIONS)} choose={value => this.onSelect(value)} />
         <Header label="statistiikka" />
-        <Stats history={history} />
+        <Statistics history={history} />
       </div>
     );
   }
